db6baee4aa09632713525651dd91b8e40982f7e6
Moldir Issenbay <m_issenbay@kbtu.kz>
2024-01-20 20:30:30 +0600

    function

---
+++ lab1/variable/4th_example.py
@@ -0,0 +1,11 @@
+var = "Meredith"
+
+def newFunction():
+    global var3
+    var3 =  "Button"
+    var2 = "Benson"
+    print(var + " is 11 years old")
+    print(var2 + "is 3 years old")
+
+newFunction()
+print("Both of them are older than " + var3)
