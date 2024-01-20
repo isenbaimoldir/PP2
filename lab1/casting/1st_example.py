0b15ab50191c86916f65c0f0072528cda18084ba
Moldir Issenbay <m_issenbay@kbtu.kz>
2024-01-20 23:35:40 +0600

    casting

---
+++ lab1/casting/1st_example.py
@@ -0,0 +1,23 @@
+x = int(1) 
+y = int(3.14) 
+z = int("5") 
+
+print(x)
+print(y)
+print(z)
+
+x = float(5)   
+y = float(41) 
+z = float("66")   
+
+print(x)
+print(y)
+print(z)
+
+x = str("1234") 
+y = str(12345)    
+z = str(98765) 
+
+print(x)
+print(y)
+print(z)
