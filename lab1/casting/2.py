06d656629f11875f98253faabf6efd3fd5fcb672
Moldir Issenbay <m_issenbay@kbtu.kz>
2024-01-22 17:34:12 +0600

    seperated

---
+++ lab1/casting/2.py
@@ -0,0 +1,7 @@
+x = float(5)   
+y = float(41) 
+z = float("66")   
+
+print(x)
+print(y)
+print(z)
---
+++ lab1/casting/3.py
@@ -0,0 +1,7 @@
+x = str("1234") 
+y = str(12345)    
+z = str(98765) 
+
+print(x)
+print(y)
+print(z)
