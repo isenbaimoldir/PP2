fc6ed08ff4f59ddaadfcbbcf4e9d7778de05ef18
Moldir Issenbay <m_issenbay@kbtu.kz>
2024-02-04 19:03:40 +0600

    rectangle and shape

--- lab3/classes/3.py
+++ lab3/classes/3.py
@@ -1,20 +1,15 @@
 class Shape:
-    def __init__(self, tarea=0):
-        self.area = tarea
+    def __init__(self):
+        pass
+    def area(self):
+        print(0)
+class Rectangle(Shape):
+    def __init__(self, length = "", width = ""):
+        self.length = int(input(length))
+        self.width = int(input(width))
+    def area(self):
+        print(self.length * self.width)
 
-    def calculate_area(self):
-        return self.area
+Rectangle().area()
+Shape().area()
 
-class Square(Shape):
-    def __init__(self, length):
-        super().__init__(length)
-
-    def calculate_area(self):
-        return self.area ** 2
-
-# Create an instance of the Square class with a side length of 5
-square_instance = Square(5)
-
-# Call the calculate_area method on both the Shape and Square instances
-print("Area of Shape:", Shape().calculate_area())          # Output: 0
-print("Area of Square:", square_instance.calculate_area())  # Output: 25
