eea789a57555e3b9b5068ea5aa9ad5af642db570
Moldir Issenbay <m_issenbay@kbtu.kz>
2024-02-21 00:29:55 +0600

    date

---
+++ lab4/topics/python dates/date.py
@@ -0,0 +1,12 @@
+import datetime
+x = datetime.datetime.now()
+print(x)
+
+print(x.year)
+print(x.strftime("%A")) #prints week days
+
+print(x.strftime("%B")) #prints full month
+
+print(x.strftime("%d")) #prints the  day
+print(x.strftime("%w")) #prints weekday as a number
+print(x.strftime("%m")) #prints months as a number
